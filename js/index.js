d7da6c199f670675b5e911c7998fef4112af352b
alemanjosh7 <josuealeman2003@gmail.com>
2021-03-22 22:43:24 -0600

    Finishing Task List

--- js/index.js
+++ js/index.js
@@ -5,7 +5,8 @@ const db = firebase.firestore(),
   btnListTasks = document.getElementById("btn-list-tasks"),
   oldForm = taskContainer.innerHTML,
   btnIndex = document.querySelector(".logo p"),
-  taskFormContainer = document.getElementById("task-form-container");
+  taskFormContainer = document.getElementById("task-form-container"),
+  selectPriority = document.getElementById("priority-select");
 const saveTask = (title, time, priority, description) =>
   db
     .collection("tasks")
@@ -17,70 +18,398 @@ const saveTask = (title, time, priority, description) =>
       description,
     });
 
-const getTasks = () => db.collection("tasks").get();
+const getTasks = () =>
+  db
+    .collection("tasks")
+    .orderBy("time")
+    .get();
 
 btnIndex.addEventListener("click", () => {
   taskContainer.innerHTML = oldForm;
+  if (taskFormContainer.classList.contains("lg:w-8/12")) {
+    taskFormContainer.classList.remove("lg:w-8/12");
+    taskFormContainer.classList.add("lg:w-4/12");
+  }
+});
+
+let statusPriority = 1;
+
+selectPriority.addEventListener("change", async (e) => {
+  statusPriority = selectPriority.value;
+  const taskList = document.getElementById("task-list");
+  taskList.innerHTML = "";
+  const queriesSnapshot = await getTasks();
+
+  if (statusPriority == 1) {
+    //high to low
+    queriesSnapshot.forEach((doc) => {
+      const task = doc.data();
+
+      const time = task.time;
+      let colorPriority;
+      if (task.priority == "High") {
+        colorPriority = "bg-red-500";
+        taskList.innerHTML += `
+        <div class=" task mx-auto bg-gradient-to-r from-gray-500 to-gray-700 m-5 shadow-xl h-72 rounded-b-lg          rounded-t-2xl md:w-full xs:w-full lg:w-full sm:w-full sm:h-full">
+          <div
+              class="upper-task flex rounded-t-xl p-2 justify-between lg:h-3/12 ${colorPriority} text-white">
+              <div class="time-task ml-4">
+                  <p>${time.substr(0, 10)} ${time.substr(11, 5)}</p>
+              </div>
+              <div class="priority-task mr-4">
+                  <p>${task.priority}</p>
+              </div>
+          </div>
+          <div
+              class="task-content text-white w-full shadow-sm flex flex-col justify-center place-items-center">
+              <div class="task-title text-center mt-3">
+                  <p>${task.title}</p>
+              </div>
+              <div class="h-1 sm:w-32 bg-white w-48 mt-4 "></div>
+              <div class="task-desc">
+                  <p class="text-center mt-5" style="max-width: 200px; word-wrap: break-word;"> ${
+                    task.description
+                  } </p>
+              </div>
+          </div>
+        </div>
+      `;
+      }
+
+      // else if (task.priority == "Normal") {
+      // colorPriority = "bg-yellow-500";
+      // } else colorPriority = "bg-green-500";
+    });
+    queriesSnapshot.forEach((doc) => {
+      const task = doc.data();
+
+      const time = task.time;
+      let colorPriority;
+      if (task.priority == "Normal") {
+        colorPriority = "bg-yellow-500";
+        taskList.innerHTML += `
+        <div class=" task mx-auto bg-gradient-to-r from-gray-500 to-gray-700 m-5 shadow-xl h-72 rounded-b-lg          rounded-t-2xl md:w-full xs:w-full lg:w-full sm:w-full sm:h-full">
+          <div
+              class="upper-task flex rounded-t-xl p-2 justify-between lg:h-3/12 ${colorPriority} text-white">
+              <div class="time-task ml-4">
+                  <p>${time.substr(0, 10)} ${time.substr(11, 5)}</p>
+              </div>
+              <div class="priority-task mr-4">
+                  <p>${task.priority}</p>
+              </div>
+          </div>
+          <div
+              class="task-content text-white w-full shadow-sm flex flex-col justify-center place-items-center">
+              <div class="task-title text-center mt-3">
+                  <p>${task.title}</p>
+              </div>
+              <div class="h-1 sm:w-32 bg-white w-48 mt-4 "></div>
+              <div class="task-desc">
+                  <p class="text-center mt-5" style="max-width: 200px; word-wrap: break-word;"> ${
+                    task.description
+                  } </p>
+              </div>
+          </div>
+        </div>
+      `;
+      }
+
+      // else if (task.priority == "Normal") {
+      // colorPriority = "bg-yellow-500";
+      // } else colorPriority = "bg-green-500";
+    });
+    queriesSnapshot.forEach((doc) => {
+      const task = doc.data();
+
+      const time = task.time;
+      let colorPriority;
+      if (task.priority == "Low") {
+        colorPriority = "bg-green-500";
+        taskList.innerHTML += `
+        <div class=" task mx-auto bg-gradient-to-r from-gray-500 to-gray-700 m-5 shadow-xl h-72 rounded-b-lg          rounded-t-2xl md:w-full xs:w-full lg:w-full sm:w-full sm:h-full">
+          <div
+              class="upper-task flex rounded-t-xl p-2 justify-between lg:h-3/12 ${colorPriority} text-white">
+              <div class="time-task ml-4">
+                  <p>${time.substr(0, 10)} ${time.substr(11, 5)}</p>
+              </div>
+              <div class="priority-task mr-4">
+                  <p>${task.priority}</p>
+              </div>
+          </div>
+          <div
+              class="task-content text-white w-full shadow-sm flex flex-col justify-center place-items-center">
+              <div class="task-title text-center mt-3">
+                  <p>${task.title}</p>
+              </div>
+              <div class="h-1 sm:w-32 bg-white w-48 mt-4 "></div>
+              <div class="task-desc">
+                  <p class="text-center mt-5" style="max-width: 200px; word-wrap: break-word;"> ${
+                    task.description
+                  } </p>
+              </div>
+          </div>
+        </div>
+      `;
+      }
+
+      // else if (task.priority == "Normal") {
+      // colorPriority = "bg-yellow-500";
+      // } else colorPriority = "bg-green-500";
+    });
+  } else if (statusPriority == 2) {
+    //low to high
+    queriesSnapshot.forEach((doc) => {
+      const task = doc.data();
+
+      const time = task.time;
+      let colorPriority;
+      if (task.priority == "Low") {
+        colorPriority = "bg-green-500";
+        taskList.innerHTML += `
+        <div class=" task mx-auto bg-gradient-to-r from-gray-500 to-gray-700 m-5 shadow-xl h-72 rounded-b-lg          rounded-t-2xl md:w-full xs:w-full lg:w-full sm:w-full sm:h-full">
+          <div
+              class="upper-task flex rounded-t-xl p-2 justify-between lg:h-3/12 ${colorPriority} text-white">
+              <div class="time-task ml-4">
+                  <p>${time.substr(0, 10)} ${time.substr(11, 5)}</p>
+              </div>
+              <div class="priority-task mr-4">
+                  <p>${task.priority}</p>
+              </div>
+          </div>
+          <div
+              class="task-content text-white w-full shadow-sm flex flex-col justify-center place-items-center">
+              <div class="task-title text-center mt-3">
+                  <p>${task.title}</p>
+              </div>
+              <div class="h-1 sm:w-32 bg-white w-48 mt-4 "></div>
+              <div class="task-desc">
+                  <p class="text-center mt-5" style="max-width: 200px; word-wrap: break-word;"> ${
+                    task.description
+                  } </p>
+              </div>
+          </div>
+        </div>
+      `;
+      }
+
+      // else if (task.priority == "Normal") {
+      // colorPriority = "bg-yellow-500";
+      // } else colorPriority = "bg-green-500";
+    });
+
+    queriesSnapshot.forEach((doc) => {
+      const task = doc.data();
+
+      const time = task.time;
+      let colorPriority;
+      if (task.priority == "Normal") {
+        colorPriority = "bg-yellow-500";
+        taskList.innerHTML += `
+        <div class=" task mx-auto bg-gradient-to-r from-gray-500 to-gray-700 m-5 shadow-xl h-72 rounded-b-lg          rounded-t-2xl md:w-full xs:w-full lg:w-full sm:w-full sm:h-full">
+          <div
+              class="upper-task flex rounded-t-xl p-2 justify-between lg:h-3/12 ${colorPriority} text-white">
+              <div class="time-task ml-4">
+                  <p>${time.substr(0, 10)} ${time.substr(11, 5)}</p>
+              </div>
+              <div class="priority-task mr-4">
+                  <p>${task.priority}</p>
+              </div>
+          </div>
+          <div
+              class="task-content text-white w-full shadow-sm flex flex-col justify-center place-items-center">
+              <div class="task-title text-center mt-3">
+                  <p>${task.title}</p>
+              </div>
+              <div class="h-1 sm:w-32 bg-white w-48 mt-4 "></div>
+              <div class="task-desc">
+                  <p class="text-center mt-5" style="max-width: 200px; word-wrap: break-word;"> ${
+                    task.description
+                  } </p>
+              </div>
+          </div>
+        </div>
+      `;
+      }
+
+      // else if (task.priority == "Normal") {
+      // colorPriority = "bg-yellow-500";
+      // } else colorPriority = "bg-green-500";
+    });
+    queriesSnapshot.forEach((doc) => {
+      const task = doc.data();
+
+      const time = task.time;
+      let colorPriority;
+      if (task.priority == "High") {
+        colorPriority = "bg-red-500";
+        taskList.innerHTML += `
+        <div class=" task mx-auto bg-gradient-to-r from-gray-500 to-gray-700 m-5 shadow-xl h-72 rounded-b-lg          rounded-t-2xl md:w-full xs:w-full lg:w-full sm:w-full sm:h-full">
+          <div
+              class="upper-task flex rounded-t-xl p-2 justify-between lg:h-3/12 ${colorPriority} text-white">
+              <div class="time-task ml-4">
+                  <p>${time.substr(0, 10)} ${time.substr(11, 5)}</p>
+              </div>
+              <div class="priority-task mr-4">
+                  <p>${task.priority}</p>
+              </div>
+          </div>
+          <div
+              class="task-content text-white w-full shadow-sm flex flex-col justify-center place-items-center">
+              <div class="task-title text-center mt-3">
+                  <p>${task.title}</p>
+              </div>
+              <div class="h-1 sm:w-32 bg-white w-48 mt-4 "></div>
+              <div class="task-desc">
+                  <p class="text-center mt-5" style="max-width: 200px; word-wrap: break-word;"> ${
+                    task.description
+                  } </p>
+              </div>
+          </div>
+        </div>
+      `;
+      }
+
+      // else if (task.priority == "Normal") {
+      // colorPriority = "bg-yellow-500";
+      // } else colorPriority = "bg-green-500";
+    });
+  } else {
+    //3 = high, 4 = normal, 5 = low
+    if (statusPriority == 3) {
+      queriesSnapshot.forEach((doc) => {
+        const task = doc.data();
+
+        const time = task.time;
+        let colorPriority;
+        if (task.priority == "High") {
+          colorPriority = "bg-red-500";
+          taskList.innerHTML += `
+          <div class=" task mx-auto bg-gradient-to-r from-gray-500 to-gray-700 m-5 shadow-xl h-72 rounded-b-lg          rounded-t-2xl md:w-full xs:w-full lg:w-full sm:w-full sm:h-full">
+            <div
+                class="upper-task flex rounded-t-xl p-2 justify-between lg:h-3/12 ${colorPriority} text-white">
+                <div class="time-task ml-4">
+                    <p>${time.substr(0, 10)} ${time.substr(11, 5)}</p>
+                </div>
+                <div class="priority-task mr-4">
+                    <p>${task.priority}</p>
+                </div>
+            </div>
+            <div
+                class="task-content text-white w-full shadow-sm flex flex-col justify-center place-items-center">
+                <div class="task-title text-center mt-3">
+                    <p>${task.title}</p>
+                </div>
+                <div class="h-1 sm:w-32 bg-white w-48 mt-4 "></div>
+                <div class="task-desc">
+                    <p class="text-center mt-5" style="max-width: 200px; word-wrap: break-word;"> ${
+                      task.description
+                    } </p>
+                </div>
+            </div>
+          </div>
+        `;
+        }
+
+        // else if (task.priority == "Normal") {
+        // colorPriority = "bg-yellow-500";
+        // } else colorPriority = "bg-green-500";
+      });
+    } else if (statusPriority == 4) {
+      queriesSnapshot.forEach((doc) => {
+        const task = doc.data();
+
+        const time = task.time;
+        let colorPriority;
+        if (task.priority == "Normal") {
+          colorPriority = "bg-yellow-500";
+          taskList.innerHTML += `
+          <div class=" task mx-auto bg-gradient-to-r from-gray-500 to-gray-700 m-5 shadow-xl h-72 rounded-b-lg          rounded-t-2xl md:w-full xs:w-full lg:w-full sm:w-full sm:h-full">
+            <div
+                class="upper-task flex rounded-t-xl p-2 justify-between lg:h-3/12 ${colorPriority} text-white">
+                <div class="time-task ml-4">
+                    <p>${time.substr(0, 10)} ${time.substr(11, 5)}</p>
+                </div>
+                <div class="priority-task mr-4">
+                    <p>${task.priority}</p>
+                </div>
+            </div>
+            <div
+                class="task-content text-white w-full shadow-sm flex flex-col justify-center place-items-center">
+                <div class="task-title text-center mt-3">
+                    <p>${task.title}</p>
+                </div>
+                <div class="h-1 sm:w-32 bg-white w-48 mt-4 "></div>
+                <div class="task-desc">
+                    <p class="text-center mt-5" style="max-width: 200px; word-wrap: break-word;"> ${
+                      task.description
+                    } </p>
+                </div>
+            </div>
+          </div>
+        `;
+        }
+
+        // else if (task.priority == "Normal") {
+        // colorPriority = "bg-yellow-500";
+        // } else colorPriority = "bg-green-500";
+      });
+    } else {
+      queriesSnapshot.forEach((doc) => {
+        const task = doc.data();
+
+        const time = task.time;
+        let colorPriority;
+        if (task.priority == "Low") {
+          colorPriority = "bg-green-500";
+          taskList.innerHTML += `
+          <div class=" task mx-auto bg-gradient-to-r from-gray-500 to-gray-700 m-5 shadow-xl h-72 rounded-b-lg          rounded-t-2xl md:w-full xs:w-full lg:w-full sm:w-full sm:h-full">
+            <div
+                class="upper-task flex rounded-t-xl p-2 justify-between lg:h-3/12 ${colorPriority} text-white">
+                <div class="time-task ml-4">
+                    <p>${time.substr(0, 10)} ${time.substr(11, 5)}</p>
+                </div>
+                <div class="priority-task mr-4">
+                    <p>${task.priority}</p>
+                </div>
+            </div>
+            <div
+                class="task-content text-white w-full shadow-sm flex flex-col justify-center place-items-center">
+                <div class="task-title text-center mt-3">
+                    <p>${task.title}</p>
+                </div>
+                <div class="h-1 sm:w-32 bg-white w-48 mt-4 "></div>
+                <div class="task-desc">
+                    <p class="text-center mt-5" style="max-width: 200px; word-wrap: break-word;"> ${
+                      task.description
+                    } </p>
+                </div>
+            </div>
+          </div>
+        `;
+        }
+
+        // else if (task.priority == "Normal") {
+        // colorPriority = "bg-yellow-500";
+        // } else colorPriority = "bg-green-500";
+      });
+    }
+  }
 });
 
 btnListTasks.addEventListener("click", async (e) => {
-  taskContainer.innerHTML = "";
-  taskContainer.innerHTML = `<form class="flex flex-col min-w-full justify-center text-center">
-    <p class="text-2xl mt-4">Pending Tasks</p>
-    <div class="priority-options mt-10">
-        <select id="cars" name="cars">
-            <option value="high-to-low">High to Low</option>
-            <option value="low-to-high">Low to High</option>
-            <option value="just-high">High</option>
-            <option value="just-normal">Normal</option>
-            <option value="just-low">Low</option>
-        </select>
-    </div>
-</form>
-<div class="task-list w-full grid lg:grid-cols-2 sm:grid-cols-1 gap-3  mx-auto p-2" id="task-list"></div>`;
   if (taskFormContainer.classList.contains("lg:w-4/12")) {
     taskFormContainer.classList.remove("lg:w-4/12");
     taskFormContainer.classList.add("lg:w-8/12");
   }
-  const queriesSnapshot = await getTasks();
 
-  queriesSnapshot.forEach((doc) => {
-    const task = doc.data();
-    console.log(task.title);
-    const taskList = document.getElementById("task-list");
-    let colorPriority;
-    if (task.priority == "High") {
-      colorPriority = "bg-red-500";
-    } else if (task.priority == "Normal") {
-      colorPriority = "bg-yellow-500";
-    } else colorPriority = "bg-green-500";
-    const time = task.time;
-    taskList.innerHTML += `
-      <div class=" task mx-auto bg-gradient-to-r from-gray-500 to-gray-700 m-5 shadow-xl h-72 rounded-b-lg          rounded-t-2xl md:w-full xs:w-full lg:w-full sm:w-full sm:h-full">
-        <div
-            class="upper-task flex rounded-t-xl p-2 justify-between lg:h-3/12 ${colorPriority} text-white">
-            <div class="time-task ml-4">
-                <p>${time.substr(0, 10) + " " + time.substr(11, 5)}</p>
-            </div>
-            <div class="priority-task mr-4">
-                <p>${task.priority}</p>
-            </div>
-        </div>
-        <div
-            class="task-content text-white w-full shadow-sm flex flex-col justify-center place-items-center">
-            <div class="task-title text-center mt-3">
-                <p>${task.title}</p>
-            </div>
-            <div class="h-1 sm:w-32 bg-white w-48 mt-4 "></div>
-            <div class="task-desc">
-                <p class="text-center mt-5" style="max-width: 200px; word-wrap: break-word;"> ${
-                  task.description
-                } </p>
-            </div>
-        </div>
-      </div>
-    `;
-  });
+  const formPriority = document.getElementById("form-select-priority");
+  const formCreateTask = document.getElementById("form-create-task");
+  if (formPriority.classList.contains("invisible")) {
+    formCreateTask.classList.toggle("invisible");
+    formCreateTask.classList.toggle("hidden");
+    formPriority.classList.toggle("invisible");
+    formPriority.classList.toggle("hidden");
+  }
 });
 
 btnCreateTask.addEventListener("click", () => {
